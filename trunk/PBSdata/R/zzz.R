a8dc4cde4eac4a06f652d68640c2d94d91e07226
rowan.haigh@dfo-mpo.gc.ca <rowan.haigh@dfo-mpo.gc.ca@037f2d45-bff9-47a2-a002-ce9e9d45618a>
2013-01-22 22:47:54 +0000

    Added new data object `ssid` that summarises survey series information in GFBioSQL.

--- trunk/PBSdata/R/zzz.R
+++ trunk/PBSdata/R/zzz.R
@@ -1,3 +1,6 @@
+# Taking cue from Roger Bivand's maptools:
+.PBSdataEnv <- new.env(FALSE, parent=globalenv())  # be sure to exportPattern("^\\.PBS") in NAMESPACE
+
 .onLoad <- function(lib, pkg)
 {
 	pkg_info <- utils::sessionInfo( package="PBSdata" )$otherPkgs$PBSdata
@@ -10,7 +13,7 @@
 	
 	packageStartupMessage("
 -----------------------------------------------------------
-PBS Data ", pkg_info$Version, " -- Copyright (C) 2004-2012 Fisheries and Oceans Canada
+PBS Data ", pkg_info$Version, " -- Copyright (C) 2004-2013 Fisheries and Oceans Canada
 (Data objects for PBS packages)
 
 A complete user guide 'PBSdata-UG.pdf' is located at 
@@ -18,8 +21,14 @@ A complete user guide 'PBSdata-UG.pdf' is located at
 
 Packaged on ", pkg_date, "
 Pacific Biological Station, Nanaimo
+
+All available PBS packages can be found at
+http://code.google.com/p/pbs-software/
 -----------------------------------------------------------
 
 ")
+}
+.onUnload <- function(libpath) {
+	rm(.PBSdataEnv)
 }
 
