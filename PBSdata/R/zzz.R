a10b156756dbbc869bb9dff68e08d5b1c6fa9b46
rowan.haigh <rowan.haigh@dfo-mpo.gc.ca>
2014-01-21 20:38:50 +0000

    (1) Added new PolyData object `seamounts` that give the approximate location of common seamounts off the BC coast. (2) Added new PolySet object `bowie` which outlines the Bowie Seamount Marine Protected Area. (3) Added two new PolySet objects `farmsFF` (finfish tenures) and `farmsSF` (shellfish tenures) from the BC government.

--- PBSdata/R/zzz.R
+++ PBSdata/R/zzz.R
@@ -10,10 +10,11 @@
 		pkg_date  <- date()
 	
 	userguide_path <- system.file( "doc/PBSdata-UG.pdf", package = "PBSdata")
-	
+	year <- substring(date(),nchar(date())-3,nchar(date()))
+
 	packageStartupMessage("
 -----------------------------------------------------------
-PBS Data ", pkg_info$Version, " -- Copyright (C) 2004-2013 Fisheries and Oceans Canada
+PBS Data ", pkg_info$Version, " -- Copyright (C) 2004-",year," Fisheries and Oceans Canada
 (Data objects for PBS packages)
 
 A complete user guide 'PBSdata-UG.pdf' is located at 
